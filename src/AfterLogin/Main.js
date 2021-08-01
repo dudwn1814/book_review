f3fda6d78cfa42c05f9e31c9cdc200ee1e350cf5
AYJ <dudwn1814@naver.com>
2021-08-01 23:14:16 +0900

    Add modal popup_2

--- src/AfterLogin/Main.js
+++ src/AfterLogin/Main.js
@@ -3,14 +3,14 @@ import ModalBtn from './ModalBtn';
 import styled from 'styled-components';
 
 //style 적용 안됨ㅜ
-const modalButton = styled.button`
-    display: "flex";
-    justifyContent: "center";
-    alignItems: "center";
-    position: "fixed";
-    bottom: 30;
-    left: 0;
-    width: "100%";
+const ModalButton = styled.button`
+    display: flex;
+    justifyContent: center;
+    alignItems: center;
+    position: fixed;
+    bottom: 30px;
+    left: 5px;
+    width: 10%;
     height: 50;
 `;
 
@@ -34,7 +34,7 @@ function Main(){
         <div>
             <h1>로그인 후 페이지</h1>
             <div>
-                <modalButton onClick={showModal}> 작성하기 </modalButton>
+                <ModalButton onClick={showModal}> 작성하기 </ModalButton>
                 <ModalBtn visible={isModalVisible} onOk={handleOk} header="도서 정보">
                     모달 팝업 창 내용
                 </ModalBtn>    
